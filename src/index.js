1384b7e0cfcc9fa4d699e3edf8fbea273360f8ed
ideepakcse <ideepakcse@gmail.com>
2023-07-07 00:51:35 +0530

    sucessfully connected to MongoDB using mongoose ODM

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express=require('express');
 
 const bodyParser = require('body-parser');
 
-const {ServerConfig}=require('./config');
+const {DatabaseConfig,ServerConfig}=require('./config');
 
 const apiRoutes=require('./routes');
 
@@ -13,8 +13,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/api',apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await DatabaseConfig.connect();
+    console.log('Mongo-db connected');
 });
 
 

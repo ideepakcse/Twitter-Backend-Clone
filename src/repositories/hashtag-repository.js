7f039f41cffa0883dcd6d6fb3d23353fc92588e7
ideepakcse <ideepakcse@gmail.com>
2023-07-07 17:41:25 +0530

    implemented the logic of storing hashtags within a tweet

--- src/repositories/hashtag-repository.js
+++ src/repositories/hashtag-repository.js
@@ -6,6 +6,26 @@ class HashtagRepository extends CrudRepository{
     constructor(){
         super(Hashtag);
     }
+
+    async bulkCreate(data) {
+        try {
+            const tags = await Hashtag.insertMany(data);
+            return tags;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async findByName(titleList) {
+        try {
+            const tags = await Hashtag.find({
+                title: titleList
+            });
+            return tags;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 module.exports=HashtagRepository;
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,14 +1,28 @@
-const {TweetRepository} = require('../repositories');
+const {TweetRepository,HashtagRepository} = require('../repositories');
 
 class TweetService {
     constructor() {
         this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
     }
 
     async create(data)
     {
         try{
             const tweet = await this.tweetRepository.create(data);
+            const content = data.content;
+            const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag) => tag.substring(1).toLowerCase()); // this regex extracts hashtags
+            let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+            let titleOfPresenttags = alreadyPresentTags.map((tag) => tag.title);
+            let newTags = tags.filter((tag) => !titleOfPresenttags.includes(tag));
+            newTags = newTags.map((tag) => {
+                return {title: tag, tweets: [tweet.id]}
+            });
+            await this.hashtagRepository.bulkCreate(newTags);
+            alreadyPresentTags.map((tag) => {
+                tag.tweets.push(tweet.id);
+                tag.save();
+            });
             return tweet;
         }
         catch(error){
